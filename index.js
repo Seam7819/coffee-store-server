3839a1cf2f62f6f6efac5fefe63782a6b1399a7d
Snu Seam <siamahme766@gmail.com>
2025-08-03 16:53:35 +0600

    commit post and get

--- index.js
+++ index.js
@@ -1,16 +1,67 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
+require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.4rme0sq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    
+    
+
+    const coffeeCollection = client.db("coffeeDB").collection("coffeeCollection")
+
+    app.get('/addCoffee',async(req,res)=>{
+      const cursor = coffeeCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+    app.post('/addCoffee', async(req, res) => {
+      const addCoffee = req.body;
+      const result = await coffeeCollection.insertOne(addCoffee)
+      res.send(result)
+    })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.send('Coffee server is running');
+app.get('/', (req, res) => {
+  res.send('Coffee server is running');
 })
 
-app.listen(port,()=>{
-    console.log(`coffee server is running in port ${port}`);
+app.listen(port, () => {
+  console.log(`coffee server is running in port ${port}`);
 })
